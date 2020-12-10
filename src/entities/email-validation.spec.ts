50383d190dce5f11a2c311bd581f1b66dcb4cf53
Bruno Rasera <bruno.rasera@unifesp.br>
2020-12-10 10:49:35 -0300

    test: validate emails with empty local part

--- src/entities/email-validation.spec.ts
+++ src/entities/email-validation.spec.ts
@@ -27,5 +27,9 @@ describe('Email validation', () => {
   test('should not accept local part larger than 64 chars', () => {
     const email = 'l'.repeat(65) + '@mail.com'
     expect(Email.validate(email)).toBeFalsy()
+
+  test('should not accept empty local part', () => {
+    const email = '@mail.com'
+    expect(Email.validate(email)).toBeFalsy()
   })
 })
